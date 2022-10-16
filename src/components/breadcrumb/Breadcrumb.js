df825ceb149f52f0185f517f1c4995bbc21e4977
Eman <emanwork369@gmail.com>
2022-10-16 20:26:09 +0200

    Update

--- src/components/breadcrumb/Breadcrumb.js
+++ src/components/breadcrumb/Breadcrumb.js
@@ -6,16 +6,16 @@ const BreadCrumb = ({ productName }) => {
             <div className="container">
                 <ol className="breadcrumb">
                     <li className="breadcrumb-item">
-                        <Link to="Task/men">Men</Link>
+                        <Link to="/Task/men">Men</Link>
                     </li>
                     <li className="breadcrumb-item">
-                        <Link to="Task/clothing">Clothing</Link>
+                        <Link to="/Task/clothing">Clothing</Link>
                     </li>
                     <li className="breadcrumb-item">
-                        <Link to="Task/Tops">Tops</Link>
+                        <Link to="/Task/Tops">Tops</Link>
                     </li>
                     <li className="breadcrumb-item">
-                        <Link to="Task/adidas">Adidas</Link>
+                        <Link to="/Task/adidas">Adidas</Link>
                     </li>
                     <li className="breadcrumb-item active" aria-current="page">{productName}</li>
                 </ol>
